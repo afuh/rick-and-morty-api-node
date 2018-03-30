6071c6f6d5199182661ad6d214e93fedc20ccb9f
Axel <axelfuh@gmail.com>
2018-03-30 19:33:04 +0200

    Add base client

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const axios = require('axios')
+
+const api = axios.create({
+  method: 'get',
+  baseURL: 'https://rickandmortyapi.com/api/',
+  timeout: 5000
+})
+
+const validate = qry => {
+  if (typeof qry === 'number' && Number.isInteger(qry)) {
+    return `/${qry}`
+  }
+
+  if (typeof qry === 'object') {
+    return `/?${Object.keys(qry)
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(qry[key])}`)
+      .join('&')}`
+  }
+
+  throw new Error(`As argument use an object, integer or nothing`)
+}
+
+const get = async (endpoint = '', opt = {}) => {
+  const query = validate(opt)
+
+  try {
+    const { data } = await api(endpoint + query)
+    return data
+  } catch (e) {
+    console.log(e.message)
+    return {
+      status: e.response.status,
+      error: e.response.data.error
+    }
+  }
+}
+
+exports.getEndpoints = () => get()
+exports.getCharacter = (opt = {}) => get('character', opt)
+exports.getLocation = (opt = {}) => get('location', opt)
+exports.getEpisode = (opt = {}) => get('episode', opt)
