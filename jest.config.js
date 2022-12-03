71ae586562f125d40d5165508e40ae96242d9f5b
Axel Fuhrmann <axelfuh@duck.com>
2022-12-03 15:06:12 +0100

    V2 (#23)
    * chore: bump packages
    
    * feat: deprecate http module in favor of fetch

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,9 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   coveragePathIgnorePatterns: ['<rootDir>/src/index.ts'],
   roots: ['<rootDir>/src'],
   testMatch: ['**/?(*.)+(spec|test).ts'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
-  },
-  globals: {
-    'ts-jest': {
-      isolatedModules: true,
-    },
   },
 }
