efb9b3b6bda11b9b8721def318d3b003f0a232ef
Axel <axel.fuhrmann@up42.com>
2022-12-04 11:19:56 +0100

    chore: remove jest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-  coveragePathIgnorePatterns: ['<rootDir>/src/index.ts'],
-  roots: ['<rootDir>/src'],
-  testMatch: ['**/?(*.)+(spec|test).ts'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-}
