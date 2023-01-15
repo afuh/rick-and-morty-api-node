6acc647d26da3929b3660de5d2af791da51270f7
Shubham Parihar <shubhamparihar391@gmail.com>
2023-01-15 21:27:20 +0530

    types: re-export interfaces (#32)

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { getCharacters, getCharacter } from './character'
 export { getLocations, getLocation } from './location'
 export { getEpisodes, getEpisode } from './episode'
 export { getEndpoints } from './endpoints'
+export * from './interfaces';
